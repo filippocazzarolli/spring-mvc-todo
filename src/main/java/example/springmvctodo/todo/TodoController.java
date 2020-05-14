738eeb9e4b86a46b23a53c344312f14caae8220c
filippocazzarolli <f.cazzarolli@ifinet.it>
2020-05-14 22:33:43 +0200

    wip

--- src/main/java/example/springmvctodo/todo/TodoController.java
+++ src/main/java/example/springmvctodo/todo/TodoController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Date;
 
@@ -33,5 +34,12 @@ public class TodoController {
         todoService.addTodo("in28Minutes", name, new Date(), false);
 //        model.clear();// to prevent request parameter "name" to be passed
         return "redirect:/";
+    }
+
+    @GetMapping("/testjson")
+    @ResponseBody
+    public String testJson() {
+        return "hello world";
+
     }
 }
